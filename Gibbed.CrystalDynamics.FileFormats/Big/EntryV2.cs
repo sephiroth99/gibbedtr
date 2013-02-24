9628a7d364ec06ff9b51bb291f7f8513f19cc007
sephiroth99 <sephiroth99@gmail.com>
2013-02-24 17:31:36 -0500

    New project UnpackTiger and associated file formats

---
+++ Gibbed.CrystalDynamics.FileFormats/Big/EntryV2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gibbed.CrystalDynamics.FileFormats.Big
+{
+    public class EntryV2
+    {
+        public uint NameHash;
+        public uint Size;
+        public uint File;
+        public uint Offset;
+
+        /// <summary>
+        /// Locale is a bitmask representing what languages this resource is
+        /// valid for. 'Default' indicates all languages.
+        /// 
+        /// Typically languages that are not implemented will have their bits set
+        /// for all non-'Default' resources.
+        /// </summary>
+        public uint Locale;
+
+        public override string ToString()
+        {
+            return string.Format("{0:X8}:{1:X8} @ {2} ({3} bytes)",
+                this.NameHash,
+                this.Locale,
+                this.Offset,
+                this.Size);
+        }
+    }
+}
